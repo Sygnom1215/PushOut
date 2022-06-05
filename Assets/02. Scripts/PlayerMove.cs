a1209c0daceca402f9f6c0108f0d3a2ce756a7d3
박소형 <77712386+Sygnom1215@users.noreply.github.com>
2022-06-06 01:07:44 +0900

    06/06 Use Gravity 켰는데 안 떨어짐 진짜 이해할 수가 없는

--- Assets/02. Scripts/PlayerMove.cs
+++ Assets/02. Scripts/PlayerMove.cs
@@ -3,23 +3,30 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float MoveSpeed;
+    public float moveSpeed;
+    private Animator anim;
     Vector3 lookDirection;
 
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) ||
-            Input.GetKey(KeyCode.RightArrow) ||
-            Input.GetKey(KeyCode.UpArrow) ||
-            Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
         {
             float forwardKeyValue = Input.GetAxis("Vertical");
             float sideKeyValue = Input.GetAxis("Horizontal");
+            anim.SetBool("IsWalk", true);
+
             lookDirection = forwardKeyValue * Vector3.forward + sideKeyValue * Vector3.right;
 
             transform.rotation = Quaternion.LookRotation(lookDirection);
-            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
+        else
+            anim.SetBool("IsWalk", false);
+
     }
 
 }
